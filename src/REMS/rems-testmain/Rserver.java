79c6c882e8ff1e1d0d3a7c67c5e9641e64e09dc2
daxeshmehra <daxeshmehrq30@gmail.com>
2015-10-25 03:26:23 +0530

    socket code but port 9001 not working

---
+++ src/REMS/rems-testmain/Rserver.java
@@ -0,0 +1,47 @@
+package main;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.*;
+
+public class Rserver {
+	
+	
+	
+	public static void main(String args[])
+	
+	{
+		
+		
+try{
+	 
+	 ServerSocket ss = new ServerSocket(8080);
+ 
+	 Socket con = ss.accept();
+
+
+	 System.out.println("Just connected to "
+             + con.getRemoteSocketAddress());
+       DataInputStream in =
+             new DataInputStream(con.getInputStream());
+       System.out.println(in.readUTF());
+       DataOutputStream out =new DataOutputStream(con.getOutputStream());
+       out.writeUTF("Thank you for connecting to "+ con.getLocalSocketAddress() + "\nGoodbye!");
+     
+       con.close();
+
+
+
+
+
+
+
+
+
+}catch(Exception e)
+{
+	System.out.println(e.getLocalizedMessage());
+}
+
+	}
+}
