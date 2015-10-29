a289d9e182132e3786c2bb69faf9ccfa47f33a4a
Sunil Panchal <sd.panchal93@gmail.com>
2015-10-28 17:52:51 -0700

    Created a skeleton for the lsit of REMS commands (10/28/15)

---
+++ src/REMS/refrencecode/testREMSCommands.java
@@ -0,0 +1,21 @@
+package REMS;
+
+public class testREMSCommands {
+
+	public static void main(String[] args) {
+		
+		REMSCommands commands = new REMSCommands(Commands.REMS_GROUDTEMP_MIN);
+		commands.remsCommands();
+		
+		REMSCommands commands1 = new REMSCommands(Commands.REMS_ULTRAVIOLET);
+		commands1.remsCommands();
+		
+		REMSCommands commands2 = new REMSCommands(Commands.REMS_AIRTEMP_MIN);
+		commands2.remsCommands();
+		
+		REMSCommands commands3 = new REMSCommands(Commands.REMS_WINDSPEED_MIN);
+		commands3.remsCommands();
+
+	}
+
+}
