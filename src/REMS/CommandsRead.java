edb637a6772bf641e1edc9669e5081187815acd9
daxeshmehra <daxeshmehrq30@gmail.com>
2015-11-11 08:14:05 +0530

    read commands from text file

---
+++ src/REMS/CommandsRead.java
@@ -0,0 +1,51 @@
+package sockets;
+
+
+import java.io.*;
+
+public class CommandsRead {
+
+	
+	static String path="C:/Users/daxesh/Documents/cmdline.txt";
+	static String cmd[] = new String[10];
+	
+	
+	public static void main (String args[])
+	{
+		cm();
+		
+	}
+	
+	static void cm()
+	{
+		String line;
+		try{
+				FileReader file = new FileReader(path); 		
+				
+				BufferedReader read = new BufferedReader(file);
+				
+				
+				
+				System.out.println("commad list :: ");
+				int i=0;
+				while((line = read.readLine())!= null)
+				{
+					
+					cmd[i]=line;
+					System.out.println(cmd[i]);
+					i++;
+				}
+				
+				
+				
+				
+				
+		}catch(Exception e )
+		{
+
+			System.out.println("error msg is"+e.getLocalizedMessage()+e.getStackTrace());
+		}
+	}
+	
+	
+}
